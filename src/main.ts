3dda2f575b2d16a32ba4f873f2ec9ef3798b8ba4
devEthan6737 <victorchacongarcia2006@gmail.com>
2025-02-16 21:05:29 +0100

    main fixed

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import * as fs from "fs";
-import path from "path";
+// import * as fs from "fs";
+// import path from "path";
 
 import { Lexer } from "./Lexer/lexer";
 import { Parser } from "./Parser/parser";
 import { executeAST } from "./Interpreter/interpreter";
 
-const code = fs.readFileSync(path.join(__dirname, "../index.chord"), "utf-8");
-const lexer = new Lexer(code);
-const parser = new Parser(lexer.tokenize()).parse();
-executeAST(parser);
+// const code = fs.readFileSync(path.join(__dirname, "../index.chord"), "utf-8");
+// const lexer = new Lexer(code);
+// const parser = new Parser(lexer.tokenize()).parse();
+// executeAST(parser);
 
 export class DisChord {
     constructor ( code: string ) {
