8dd40341ad815a00ffcc0c4e40d7093ab6c7d366
devEthan6737 <victorchacongarcia2006@gmail.com>
2026-04-16 10:32:10 +0200

    refactor: commands parser

--- src/dischord/Parser/Commands/CommandParser.ts
+++ src/dischord/Parser/Commands/CommandParser.ts
@@ -1,5 +1,5 @@
 import { DisChordParser } from "../parser";
-import { CommandNode, CommandOptionNode, CommandParam, DisChordASTNode } from "../../types";
+import { CommandNode } from "../../types";
 import { KeyWords } from "../../../chord/keywords";
 import { SubParser } from "../subparser";
 
@@ -35,102 +35,12 @@ export default class CommandParser extends SubParser {
     parse (): CommandNode {
         this.consume('COMANDO');
         const commandName = this.consume('IDENTIFICADOR').value;
-    
-        this.consume('L_BRACE');
-    
-        const body: DisChordASTNode[] = [];
-        const params: CommandParam[] = [];
-            
-        while (this.peek().type !== 'R_BRACE') {
-            switch (this.peek().value) {
-                case 'descripcion':
-                    this.consume('IDENTIFICADOR');
-                    const value: DisChordASTNode = this.parsePrimary();
-    
-                    const param: CommandParam = {
-                        type: 'ParametroDeComando',
-                        property: 'Descripcion',
-                        value
-                    };
-    
-                    params.push(param);
-                    break;
-                case 'opciones':
-                    this.consume('IDENTIFICADOR');
-                    const options: CommandOptionNode[] = [];
-    
-                    this.consume('L_BRACE');
-                    while (this.peek().type !== 'R_BRACE') {
-                        options.push(this.parseCommandOption());
-                    }
-                    this.consume('R_BRACE');
-    
-                    const optionsParam: CommandParam = {
-                        type: 'ParametroDeComando',
-                        property: 'Opciones',
-                        options
-                    }
-    
-                    params.push(optionsParam);
-                    break;
-                default:
-                    body.push(this.parseStatement());
-            }
-        }
-    
-        this.consume('R_BRACE');
+        const body = this.parseODB();
+
         return this.createNode<CommandNode>({
             type: 'CrearComando',
             value: commandName,
-            body,
-            params
+            body
         });
-    }
-
-    /**
-     * Parses individual command options
-     * Expected structure: `nameOption: {...}`
-     * @private
-     * @returns {CommandOptionNode} The parsed option configuration.
-     * @throws {Error} If required properties are missing or types are invalid.
-     */
-    private parseCommandOption(): CommandOptionNode {
-        const name = this.consume('IDENTIFICADOR').value;
-        const option: Partial<CommandOptionNode> = {
-            type: 'ParametroDeComando',
-            name
-        };
-    
-        // this part needs to be refactor.
-        // actually, you just can set one type of option and the rest of properties in any order.
-        this.consume('L_BRACE');
-        while (this.peek().type !== 'R_BRACE') {
-            const token = this.peek();
-    
-            switch (token.value) {
-                case 'tipo':
-                    this.consume('TIPO');
-                    const type = this.parsePrimary();
-                    if (type.type != 'Literal') throw new Error(`El tipo de opción de comando debe ser un literal, se encontró '${type.type}'`);
-                    if (type.value != 'Texto') throw new Error(`Actualmente solo se soporta el tipo 'Texto' para opciones de comando, se encontró '${type.value}'`);
-                    option.property = type.value;
-                    break;
-                case 'descripcion':
-                    this.consume('IDENTIFICADOR');
-                    option.description = this.parsePrimary();
-                    break;
-                case 'requerido':
-                    this.consume('IDENTIFICADOR');
-                    option.required = this.parsePrimary();
-                    break;
-                default:
-                    throw new Error(`Dentro de las opciones de comando solo se permiten 'tipo', 'descripcion' y 'requerido', se encontró '${token.value}'`);
-            }
-        }
-        this.consume('R_BRACE');
-    
-        if (!option.property || !option.description || option.required === undefined) throw new Error(`Faltan propiedades para la opción de comando '${name}'`);
-    
-        return option as CommandOptionNode;
     }
 }
--- src/dischord/types.ts
+++ src/dischord/types.ts
@@ -19,32 +19,8 @@ export interface EventNode extends BaseNode<DisChordNodeType> {
 export interface CommandNode extends BaseNode<DisChordNodeType> {
     type: 'CrearComando';
     value: string;
-    params: CommandParam[];
-    body: DisChordASTNode[];
+    body: DisChordODBNode;
 }
-
-export interface CommandDescriptionParam extends BaseNode<DisChordNodeType> {
-    type: 'ParametroDeComando';
-    property: 'Descripcion';
-    value: DisChordASTNode;
-}
-
-export interface CommandOptionsParam extends BaseNode<DisChordNodeType> {
-    type: 'ParametroDeComando';
-    property: 'Opciones';
-    options: CommandOptionNode[];
-}
-
-export interface CommandStringOptionParam extends BaseNode<DisChordNodeType> {
-    type: 'ParametroDeComando';
-    name: string;
-    property: 'Texto';
-    description: DisChordASTNode;
-    required: DisChordASTNode;
-}
-
-export type CommandOptionNode = CommandStringOptionParam;
-export type CommandParam = CommandDescriptionParam | CommandOptionsParam;
 
 export interface MessageNode extends BaseNode<DisChordNodeType> {
     type: 'CrearMensaje';
