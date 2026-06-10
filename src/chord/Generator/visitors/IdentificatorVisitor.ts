8ed5ed29efd2701582844f140d4352e39b12c3d0
devEthan6737 <victorchacongarcia2006@gmail.com>
2026-06-10 12:22:24 +0200

    add: IdentificatorVisitor subgenerator

---
+++ src/chord/Generator/visitors/IdentificatorVisitor.ts
@@ -0,0 +1,28 @@
+import { BaseNode, IdentificatorNode, TokenType, TokenTypeUnion } from "../../types";
+import { SubGenerator } from "../SubGenerator";
+
+/**
+ * Atomic SubGenerator to process syntactic Identifier source references.
+ * @class IdentificatorVisitor
+ * @extends {SubGenerator<T, N>}
+ * @template {string} T - Extensible token type string vector.
+ * @template {BaseNode<T>} N - Extensible abstract syntax tree node layout.
+ */
+export class IdentificatorVisitor<T extends string, N extends BaseNode<T>> extends SubGenerator<T, N> {
+    /**
+     * The node type string that triggers the activation of this specific sub-generator.
+     * @public
+     * @static
+     */
+    public static triggerToken: TokenTypeUnion<TokenType> | undefined = TokenType.IDENTIFICADOR_NODO;
+
+    /**
+     * Translates an Identifier node directly into its raw string literal value representation.
+     * @param {IdentificatorNode<T>} node - The concrete identifier syntax tree node.
+     * @returns {string} The raw string naming the resolved identifier block.
+     * @public
+     */
+    public visit(node: IdentificatorNode<T>): string {
+        return node.value;
+    }
+}
