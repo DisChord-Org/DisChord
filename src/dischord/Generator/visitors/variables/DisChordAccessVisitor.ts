2e0275babe02384983e3cfe231979d3043eece65
devEthan6737 <victorchacongarcia2006@gmail.com>
2026-06-27 16:43:22 +0200

    fix: access & call dischord visitors

--- src/dischord/Generator/visitors/variables/DisChordAccessVisitor.ts
+++ src/dischord/Generator/visitors/variables/DisChordAccessVisitor.ts
@@ -1,11 +1,23 @@
 import { AccessVisitor } from "../../../../chord/Generator/visitors/variables/AccessVisitor";
 import { DisChordNodeType, DisChordNode } from "../../../types";
-import { AccessNode, TokenType } from "../../../../chord/types";
+import { AccessNode, TokenType, TokenTypeUnion } from "../../../../chord/types";
 import { corelib } from "../../../core.lib";
 
 export class DisChordAccessVisitor extends AccessVisitor<DisChordNodeType, DisChordNode> {
-    public static override triggerToken = TokenType.ACCESO;
+     /**
+     * The node type string that triggers the activation of this specific sub-generator.
+     * @public
+     * @static
+     * @override
+     */
+    public static override triggerToken: TokenTypeUnion<TokenType> | undefined = TokenType.ACCESO;
 
+     /**
+     * Overrides the visit method to provide custom code generation for property access based on the core library mappings.
+     * @override
+     * @param node The AccessNode representing a property access in the AST, containing the object and property being accessed.
+     * @returns The generated code for the property access, which may be translated based on the core library mappings or fall back to the default generation if no mapping is found.
+     */
     public override visit(node: AccessNode<DisChordNodeType, DisChordNode>): string {
         const objName = node.object.type === TokenType.IDENTIFICADOR ? node.object.value : null;
         const propName = node.property;
