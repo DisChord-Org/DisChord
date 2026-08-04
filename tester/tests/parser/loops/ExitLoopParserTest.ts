13c2eb00bdf8d240108bcf6140f0e4b514697dfd
devEthan6737 <victorchacongarcia2006@gmail.com>
2026-08-05 00:15:27 +0200

    add: more loop tests (no expected json yet)

---
+++ tester/tests/parser/loops/ExitLoopParserTest.ts
@@ -0,0 +1,49 @@
+import { Test } from "../../../Test";
+
+/**
+ * @class ExitLoopParserTest
+ * @description Validates that the parser correctly extracts an array loop.
+ */
+export class ExitLoopParserTest extends Test {
+    /**
+     * @type {string}
+     */
+    public readonly name: string = 'Exit Loop - Parser Test';
+    
+    /**
+     * @type {string}
+     */
+    public readonly description: string = 'It has to generate the loop AST correctly';
+
+    /**
+     * @type {string}
+     */
+    public readonly code: string = `
+        para (valor en rango(10)) {
+            si (valor igual 5) {
+                salir
+            }
+            consola.imprimir(valor)
+        }
+    `;
+
+    /**
+     * @type {string} Snapshot strictly mapped to match the actual lexer coordinates produced by the indented template string.
+     */
+    public readonly expected: string = JSON.stringify([
+    ], null, 2);
+
+    /**
+     * @method run
+     * @returns {void}
+     * @public
+     * @override
+     */
+    public run(): void {
+        const context = this.createMockContext(this.code);
+        const lexed = this.lex(context);
+        const ast = this.parse(lexed, context);
+
+        this.assertDeepEqual(JSON.stringify(ast, null, 2), this.expected);
+    }
+}
