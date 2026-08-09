017420e32b096b8e5d5af496fcb75cca1ecb0b24
devEthan6737 <victorchacongarcia2006@gmail.com>
2026-08-09 23:44:04 +0200

    add: helloworld test

---
+++ tester/tests/Consola/SimpleLog/SimpleLogTest.ts
@@ -0,0 +1,17 @@
+import { Test } from "../../../Test";
+
+/**
+ * @class SimpleLogTest
+ * @description Validates that DisChord can detect a consola.imprimir
+ */
+export class SimpleLogTest extends Test {
+    /**
+     * @type {string}
+     */
+    public readonly name: string = 'Simple Log - Test';
+    
+    /**
+     * @type {string}
+     */
+    public readonly description: string = 'Validates that DisChord can detect a consola.imprimir';
+}
